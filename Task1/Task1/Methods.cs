f39547b0db14f763065d95fd6d709734567a230c
SabirQ <sabirmg@code.edu.az>
2022-03-01 00:24:35 +0400

    Task1_added

---
+++ Task1/Task1/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+   static class Methods
+    {
+        public static string Capitalized(this string sentence)
+        {
+            StringBuilder capitalized = new StringBuilder();
+            sentence = sentence.ToLower();
+            capitalized.Append(Char.ToUpper(sentence[0]));
+            for (int i = 1; i < sentence.Length; i++)
+            {
+                if (sentence[i]==' ')
+                {
+                    capitalized.Append(Char.ToUpper(sentence[i]));
+                    capitalized.Append(Char.ToUpper(sentence[i + 1]));
+                    i++;
+                }
+                else
+                {
+                    capitalized.Append(sentence[i]);
+                }
+            }
+            return Convert.ToString(capitalized);
+        }
+
+        
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sentence = "evERYbodY haS A cHoiCE";
+            Console.WriteLine(Sentence.Capitalized());
+
+            
+        }
+
+    }
+}
