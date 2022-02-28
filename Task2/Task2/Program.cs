3d90a55b2050f9ec2e32e9cac74cd894bc4e9104
SabirQ <sabirmg@code.edu.az>
2022-03-01 00:27:58 +0400

    Task2_added

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sentence = "No pain no gain let us train";
+            string SearchWord = "gain";
+            
+            if (Sentence.FindOut(SearchWord))
+            {
+                Console.WriteLine($"Verilmish cumlede '{SearchWord}' sozu movcuddur ");
+            }
+            else
+            {
+                Console.WriteLine($"Verilmish cumlede '{SearchWord}' sozu movcud deyil ");
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Searching.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    static class Searching
+    {
+        public static bool FindOut(this string sentence,string word)
+        {
+            bool result = false;
+            string[] Array = sentence.Split(' ');
+
+            foreach (string element in Array)
+            {
+                if (word==element)
+                {
+                    result = true;
+                }
+            }
+            return result;            
+        }
+    }
+}
